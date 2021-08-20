66a6a0374a8cc9ddee9845b67d8fb6fd042f906c
Captain Skywalker <murlidharsingh1319@gmail.com>
2021-08-20 09:56:02 +0530

    Basics

--- 5-First-C-Programs/exercise-2.c
+++ 5-First-C-Programs/exercise-2.c
@@ -7,5 +7,15 @@ int main(int argc, char const *argv[])
     printf("     ***     \n");
     printf("    *****     \n\n");
 
+
+    // First aim is ASAI Inc...
+    /* 
+    
+    Galactic Human Empire...
+
+                >_ Captain Skywalker
+    
+    */
+   
     return 0;
 }
