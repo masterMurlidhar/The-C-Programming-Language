671bcfb2dc10bf9bbc88409e602957624b04ab78
Captain Skywalker <murlidharsingh1319@gmail.com>
2021-08-20 09:28:36 +0530

    Basics

---
+++ 5-First-C-Programs/exercise-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("An Isosceles Triangle...\n\n");
+    printf("      *     \n");
+    printf("     ***     \n");
+    printf("    *****     \n\n");
+
+    return 0;
+}
